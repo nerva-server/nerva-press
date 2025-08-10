14135e659678bbc1529069286949e6b09d229d9d
lahit1 <naifcanbasci@gmail.com>
2025-08-10 19:13:24 +0300

    First ever program, yup yup

---
+++ include/nerva-press/temper.hxx
@@ -0,0 +1,10 @@
+#include <iostream>
+
+namespace Temper {
+
+void use(std::istream &input_stream);
+void use(std::ostream &output_stream);
+
+int proc();
+
+}
---
+++ src/nerva-press/temper.cxx
@@ -0,0 +1,51 @@
+#include<nerva-press/temper.hxx>
+
+namespace Temper {
+
+std::istream* in;
+std::ostream* out;
+
+void use(std::istream &input_stream) {
+	in = &input_stream;
+}
+void use(std::ostream &output_stream) {
+	out = &output_stream;
+}
+
+
+bool isCode=false;
+
+void checkCode();
+
+int proc() {
+	*out << "res << R\"(";
+	while(in->peek() != EOF) {
+		if(!isCode && in->peek() == '{') {
+			in->get();
+			if(in->peek() == '{') {
+				isCode = true;
+				in->get();
+				*out << ")\";" << std::endl;
+			} else *out << '{';
+		} else if(isCode && in->peek() == '}') {
+                        in->get();
+                        if(in->peek() == '}') {
+				isCode = false;
+                                in->get();
+                                *out << std::endl <<
+					"res << R\"(";
+                        } else *out << '}';
+                } else *out << (char)in->get();
+	}
+
+	if(isCode) {
+		std::cerr << "Code block isn't closed" << std::endl;
+		return 1;
+	}
+
+	*out << ")\";" << std::endl;
+
+	return 0;
+}
+
+}
